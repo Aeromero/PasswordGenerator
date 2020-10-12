737f414ba9597376bf3acf84fced0d6fe3d30111
Ana Espinoza <aespinozaromero1@gmail.com>
2020-10-11 19:05:34 -0500

    commit main

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment code here
 
+// Prompt for Password
 var enter;
 var length = prompt("How many characters would you like your password to be?")
 if (length < 8 || length > 128){
@@ -30,10 +31,26 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(Uppercase, Lowercase, Numbers, Symbols, length) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var password = "";
   passwordText.value = password;
 };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var writePassword = (getRandomLowercase + getRandomUppercase + getRandomNumber)
+
+function getRandomUppercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+} 
+
+function getRandomLowercase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+
+}
+
 
